f22b234205331c7b362063760d22e9ed1bfec90f
jalcantararivera <jalcantararivera@outlook.com>
2026-04-16 23:36:27 -0500

    finalizando dag

--- dags/ventas_etl.py
+++ dags/ventas_etl.py
@@ -2,6 +2,7 @@ import pandas as pd
 from airflow.decorators import dag, task
 from datetime import datetime
 from pathlib import Path
+from sklearn.preprocessing import MinMaxScaler,LabelEncoder
 import os
 
 
@@ -16,18 +17,74 @@ def ventas_etl():
     data_dir = Path("/opt/airflow/data")
     
     @task 
-    def leer_archivo() -> pd.DataFrame:
-        df=pd.read_excel(os.path.join(data_dir,"bd_ventas.xlsx"))
-        return df 
-        
+    def leer_archivo_ventas() -> pd.DataFrame:
+        df_ventas=pd.read_excel(os.path.join(data_dir,"bd_ventas.xlsx"),sheet_name=0)
+        return df_ventas
+    
+    @task
+    def leer_archivo_vehiculos() -> pd.DataFrame:
+        df_vehiculos= pd.read_excel(os.path.join(data_dir,"bd_ventas.xlsx"),sheet_name=1)
+        return df_vehiculos
+    
     @task 
-    def procesar_dataframe(df:pd.DataFrame) :
-        print(f"Filas procesadas: {len(df)}")
-        return "df.to_csv"
+    def procesar_dataframe(df_ventas:pd.DataFrame,df_vehiculos:pd.DataFrame) -> pd.DataFrame:
+        #CALCULO DEL COSTO ORIGINAL DEL VEHÍCULO
+        df_ventas['Precio Venta sin IGV'] = pd.to_numeric(df_ventas['Precio Venta sin IGV'], errors='coerce')
+        df_ventas['Costo Vehiculo'] = 0.6 * df_ventas['Precio Venta sin IGV']
+        #CALCULO DEL PRECIO DE VENTA REAL (MÁS IGV)
+        df_ventas['Precio Venta Real'] = df_ventas['Precio Venta sin IGV'] + 0.18 * df_ventas['Precio Venta sin IGV']
+        #OBTENCIÓN DE LA SEDE
+        df_ventas[['Distrito', 'Provincia', 'Departamento']] = df_ventas['Ubicación'].str.split(',', expand=True)
+        df_ventas=df_ventas.rename(columns={'Distrito': 'Sede'}) #Renombre de la columna a sede
+        #RELIZAMOS UN JOIN DE LOS DF VENTAS Y VEHICULOS
+        df_ventas = pd.merge(df_ventas, df_vehiculos,left_on='ID_Vehículo',right_on='ID_Vehiculo', how='inner')
+        df= df_ventas.drop(columns=['Ubicación', 'ID_Vehículo','ID_Vehiculo','Provincia','Departamento','Cliente','MARCA','MODELO','Vendedor'])
+        df.set_index('ID', inplace=True)
+        #NORMALIZACIÓN DE VALORES DE VENTA Y COSTO ()
+        scaler = MinMaxScaler()
+        df['Precio Venta sin IGV'] = scaler.fit_transform(df[['Precio Venta sin IGV']])
+        df['Costo Vehiculo'] = scaler.fit_transform(df[['Costo Vehiculo']])
+        df['Precio Venta Real'] = scaler.fit_transform(df[['Precio Venta Real']])
+        #TRANSFORMACIÓN LINEAL PARA EL AÑO DE FRABRICACIÓN DEL VEHÍCULO
+        le = LabelEncoder()
+        df['AÑO'] = le.fit_transform(df['AÑO'])
+        df=df.rename(columns={'AÑO': 'ano_fabricacion'}) #Renombre de la columna
+        #ONE HOT ENCODING SEGUN TIPO DE VEHICULO, SEGMENTO Y SEDE
+        df_encoded_tv = pd.get_dummies(df.TIPO_VEHÍCULO, dtype=int)
+        df_encoded_seg = pd.get_dummies(df.Segmento, dtype=int)
+        df_encoded_sede = pd.get_dummies(df.Sede, dtype=int)
+        df=pd.concat([df,df_encoded_tv],axis=1)
+        df=pd.concat([df,df_encoded_seg],axis=1)
+        df=pd.concat([df,df_encoded_sede],axis=1)
+        df= df.drop(columns=['Sede','Segmento', 'TIPO_VEHÍCULO'])
+        #LIMPIEZA DE DATOS DE PRECIO
+        #Debido a que los precios venta sin igv, costo vehiculo y precio venta real son proporcionales. Cuando se realiza la normalización nos da la misma información , por tanto se procede a la eliminación de las columnas
+        df= df.drop(columns=['Precio Venta sin IGV','Precio Venta Real'])
+        df=df.rename(columns={'Costo Vehiculo': 'Costo'}) #Renombre de la columna
+        #TARGET ENCODING PARA EL CANAL, USANDO COMO BASE LA MEDIA DEL COSTO
+        mean_map = df.groupby('Canal')['Costo'].mean()
+        df['canal_encoded'] = df['Canal'].map(mean_map)
+        df= df.drop(columns=['Canal'])
+        # Convertir la columna a formato datetime
+        df['Fecha'] = pd.to_datetime(df['Fecha'])
+        # Extraer Mes y Año en columnas nuevas
+        df['Mes'] = df['Fecha'].dt.month
+        df['Año'] = df['Fecha'].dt.year
+        # Eliminar la columna original
+        df.drop(columns=['Fecha'], inplace=True)
+        # Renombrar columnas
+        df.rename(columns={'Costo': 'costo', 'AUTO': 'auto','CAMIÓN':'camion','Año':'ano','MES':'mes','AUTOBUS':'autobus','CAMIONETA':'camioneta','Empresa':'empresa','Persona':'persona'}, inplace=True)
+        return df
 
+    @task 
+    def guardar_dataframe(df:pd.DataFrame):
+        print(df)
 
-    df= leer_archivo()
-    procesar_dataframe(df)
+    df_ventas= leer_archivo_ventas()
+    df_vehiculos= leer_archivo_vehiculos()
+    df=procesar_dataframe(df_ventas,df_vehiculos)
+    guardar_dataframe(df)
+
 
 
 dag = ventas_etl()
